fce2bb9803469b57bf92238a6facf78a87912fdb
pc casa <alex.taravilla@enti.cat>
2023-03-10 22:50:11 +0100

    seta acabada

---
+++ Assets/Scripts/Enemys/SteusConfusus/ExploSeta.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class ExploSeta : MonoBehaviour
+{
+    GameObject player;
+    public GameObject Explo;
+    bool isExploating = false;
+    PlayerStatus pst;
+    Collider2D cl;
+    public Collider2D lc;
+    public SpriteRenderer sr;
+    private float confusedTime = 5;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("mantee_v2");
+        cl = GetComponent<Collider2D>();
+        pst = player.GetComponent<PlayerStatus>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isExploating) {
+            sr.enabled = false;
+            lc.enabled = false;
+            Explo.SetActive(true);
+
+
+            pst.isConfused = true;
+            confusedTime -= Time.deltaTime;
+            
+        }
+        if (confusedTime <= 0) {
+            Destroy(gameObject);
+            pst.isConfused = false;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player") {
+            isExploating = true;
+            sr.enabled = false;
+        }
+    }
+}
--- Assets/Scripts/Enemys/SteusConfusus/setaRender.cs
+++ Assets/Scripts/Enemys/SteusConfusus/setaRender.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class setaRender : MonoBehaviour
 {
+    ExploSeta ex;
+    SpriteRenderer sr;
+    GameObject explosion;
     // Start is called before the first frame update
     void Start()
     {
-        
+        explosion = GameObject.Find("explosionP");
+        sr.GetComponent<SpriteRenderer>();
+        ex = GetComponent<ExploSeta>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+  
     }
 }
