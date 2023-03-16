eb8367457da406e50ed0e701c5561d660552e084
PC-imatge-2022-2023 <licenses@enti.cat>
2023-03-16 14:43:20 +0100

    bug de dispersion arreglado

--- Assets/Scripts/Player/GunScripts/Bullets/BulletTest.cs
+++ Assets/Scripts/Player/GunScripts/Bullets/BulletTest.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BulletTest : MonoBehaviour
 {
-    public Vector2 DispersionRange;
+    public float dispersionAngle;
     Rigidbody2D rb;
     Camera mainCam;
     Vector3 mousePos;
@@ -21,19 +21,10 @@ public class BulletTest : MonoBehaviour
         bs = GetComponent<BulletStats>();
         
 
-
-
-        randomAngle = Random.Range(DispersionRange.x, DispersionRange.y);
-        float angle = Mathf.Deg2Rad * randomAngle;
-        sen = Mathf.Sin(angle) * 2.5f;  //3; ;
-        cos = Mathf.Cos(angle) * 2.5f; //3;
-        Vector3 randomDir = new Vector3(cos, sen, 0);
-
-
         mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
         Vector3 direcion = mousePos - transform.position; // el vector desde el objeto hacia el mouse
-        Vector3 randomBullet = direcion + randomDir;      // cambio de direcion para que el arma tenga dispersion
+        Vector3 randomBullet = Quaternion.Euler(0f, 0f, Random.Range(-dispersionAngle, dispersionAngle)) * direcion;      // cambio de direcion para que el arma tenga dispersion
         Vector3 rotation = transform.position - mousePos; // la rotacion es para que la bala siempre se vea recta en todos los angulos respecto al mouse
 
         rb.velocity = new Vector2(randomBullet.x, randomBullet.y).normalized * bs.speed;
