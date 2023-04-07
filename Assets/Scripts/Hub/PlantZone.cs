0e47e1d5271d1c969bf36b829538b2e62004138c
DESKTOP-502TB91\Inazu <aleix.gonzalez@enti.cat>
2023-04-07 21:09:59 +0100

    añadiendo ayudas visuales

--- Assets/Scripts/Hub/PlantZone.cs
+++ Assets/Scripts/Hub/PlantZone.cs
@@ -11,12 +11,15 @@ public class PlantZone : MonoBehaviour
     [SerializeField] private bool inRange = false;
     public GameObject plantMenu;
     public GameObject Guns;
-   
+    public GameObject press;
+    private SpriteRenderer sr;   
 
     // Start is called before the first frame update
     void Start()
     {
-     
+        sr = press.GetComponent<SpriteRenderer>();
+        sr.enabled = false;
+
         pm = GameObject.Find("mantee_v2").GetComponent<PlayerStats>();
         sh = GameObject.Find("mantee_v2").GetComponent<ShootScript>();
         cl = GetComponent<Collider2D>();
@@ -25,10 +28,22 @@ public class PlantZone : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+
+
+        if (inRange)
+        {
+            sr.enabled = true;
+        }
+        else
+        {
+            sr.enabled = false;
+        }
+
         //mejorar esto para que solo se abra con la e y se cierre otra vez pulsando e
         if (Input.GetButtonDown("interaction") && inRange)
         {
+            press.SetActive(false);
             //desactivar el movimento
             pm.speed = 0;
 
@@ -39,6 +54,7 @@ public class PlantZone : MonoBehaviour
         }
         if (pm.speed == 0 && Input.GetKeyDown(KeyCode.Escape))
         {
+            press.SetActive(true);
             pm.speed = 5;
             Guns.SetActive(true);
             plantMenu.SetActive(false);
