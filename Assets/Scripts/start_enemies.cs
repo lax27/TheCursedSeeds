45307d245c4a71bf84145d1c804dd9e836cd1d38
PC-imatge-2022-2023 <licenses@enti.cat>
2023-04-11 11:17:02 +0200

    coll de enemigos

---
+++ Assets/Scripts/start_enemies.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class start_enemies : MonoBehaviour
+{
+    public List<GameObject> enemies = new List<GameObject>();
+
+    public List<BoxCollider2D> boxCollider2Ds = new List<BoxCollider2D>();
+    public List<Animator> tps = new List<Animator>();
+    public List<bool> enemyDead = new List<bool>();
+
+    public int nulls;
+
+    private bool enemiesExecute;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].SetActive(false);
+        }
+
+        for(int i = 0; i < boxCollider2Ds.Count; i++)
+        {
+            boxCollider2Ds[i].enabled = false;
+            for(int j = 0; j < tps.Count; j++)
+            {
+                tps[j].enabled = false;
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < enemyDead.Count; i++)
+        {
+            if (enemies[i] == null)
+            {
+                enemyDead[i] = true;
+            }
+        }
+
+ 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (enemies.Count > 0)
+        {
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                enemies[i].SetActive(true);
+            }
+        }
+        else
+        {
+            Debug.Log("All enemies executed");
+        }
+
+
+     
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (enemyDead[0] == true && enemyDead[1] == true && enemyDead[2] == true && enemyDead[3] == true && enemyDead[4] == true && enemyDead[5] == true)
+        {
+            for (int i = 0; i < tps.Count; i++)
+            {
+                tps[i].enabled = true;
+                for (int j = 0; j < tps.Count; j++)
+                {
+                    boxCollider2Ds[i].enabled = true;
+                }
+            }
+        }
+    }
+
+
+
+
+}
