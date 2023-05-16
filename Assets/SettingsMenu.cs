e77a6d20b2be6b92158991e04090f0dd498b887b
void <alex.taravilla@enti.cat>
2023-05-16 12:08:13 +0200

    a

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -5,44 +5,44 @@ using UnityEngine;
 
 public class SettingsMenu : MonoBehaviour
 {
-    //[SerializeField] GameObject settingsMenu;
-    //public GameObject livesAmount;
-    //public GameObject rightArrowButtonUI;
-    //public GameObject leftArrowButtonUI;
+    [SerializeField] GameObject settingsMenu;
+    public GameObject livesAmount;
+    public GameObject rightArrowButtonUI;
+    public GameObject leftArrowButtonUI;
 
 
 
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-        
-    //}
+    // Start is called before the first frame update
+    void Start()
+    {
 
-    //// Update is called once per frame
-    //void Update()
-    //{
-        
-    //}
+    }
 
-    //public void GameLivesUp()
-    //{
-    //    if(livesAmount = "5")
-    //        //si la cantidad de vidas es inferior a 7 suma 2
-    //        //si la cantidad de vidas es igual o superior a 7 desactiva el componente
-    //        livesAmount = 2;
+    // Update is called once per frame
+    void Update()
+    {
 
-    //}
+    }
 
-    //public void GameLivesDown()
-    //{
-    //    if (livesAmount = "5")
-    //        //si la cantidad de vidas no es igual a 3 resta 2
-    //        //si la cantidad de vidas es igual o inferior a 3 desactiva el componente
-    //        livesAmount = 2;
+    public void GameLivesUp()
+    {
+        if (livesAmount = "5")
+            //si la cantidad de vidas es inferior a 7 suma 2
+            //si la cantidad de vidas es igual o superior a 7 desactiva el componente
+            livesAmount = 2;
 
-    //}
-    //public void BackToMenu()
-    //{
-    //    settingsMenu.SetActive(false);
-    //}
+    }
+
+    public void GameLivesDown()
+    {
+        if (livesAmount = "5")
+            //si la cantidad de vidas no es igual a 3 resta 2
+            //si la cantidad de vidas es igual o inferior a 3 desactiva el componente
+            livesAmount = 2;
+
+    }
+    public void BackToMenu()
+    {
+        settingsMenu.SetActive(false);
+    }
 }
